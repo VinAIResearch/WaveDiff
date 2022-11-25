4968a9914b263a5cde5f33770dac2d689e582929
Phùng Tiến Hào <v.haopt12@vinai.io>
2022-11-25 17:29:50 +0700

    fix bash script

--- run_ddgan.sh
+++ run_ddgan.sh
@@ -16,20 +16,19 @@ if [ -z "$1" ]; then
 fi
 
 echo $DATASET $MODE $GPUS
-echo $MODE
 
 # ----------------- VANILLA -----------
 if [[ $MODE == train ]]; then
 	echo "==> Training DDGAN"
 
-	if [[ $DATASET == cifar10 ]]; then
-		python train_ddgan.py --dataset cifar10 --exp ddgan_cifar10_exp1 --num_channels 3 --num_channels_dae 128 --num_timesteps 4 \
+    if [[ $DATASET == cifar10 ]]; then
+        python train_ddgan.py --dataset cifar10 --exp ddgan_cifar10_exp1 --num_channels 3 --num_channels_dae 128 --num_timesteps 4 \
 			--num_res_blocks 2 --batch_size 256 --num_epoch 1800 --ngf 64 --nz 100 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
 			--use_ema --ema_decay 0.9999 --r1_gamma 0.02 --lr_d 1.25e-4 --lr_g 1.6e-4 --lazy_reg 15 \
 			--ch_mult 1 2 2 2 --save_content --datadir data/cifar-10 \
 			--master_port $MASTER_PORT --num_process_per_node $GPUS \
 
-	if [[ $DATASET == stl10 ]]; then
+	elif [[ $DATASET == stl10 ]]; then
 		python train_ddgan.py --dataset stl10 --exp ddgan_stl10_exp1 --num_channels 3 --num_channels_dae 128 --num_timesteps 4 \
 			--num_res_blocks 2 --batch_size 128 --num_epoch 1800 --ngf 64 --nz 100 --z_emb_dim 256 --n_mlp 4 --embedding_type positional \
 			--use_ema --ema_decay 0.9999 --r1_gamma 0.02 --lr_d 1.25e-4 --lr_g 1.6e-4 --lazy_reg 15 \
@@ -50,20 +49,18 @@ if [[ $MODE == train ]]; then
 			--save_content_every 25 \
 
 	elif [[ $DATASET == lsun ]]; then
-		python3 train_ddgan.py --dataset lsun --image_size 256 --exp ddgan_lsun_exp1 --num_channels 3 --num_channels_dae 64 --ch_mult 1 1 2 2 4 4 --num_timesteps 4 \
+		python train_ddgan.py --dataset lsun --image_size 256 --exp ddgan_lsun_exp1 --num_channels 3 --num_channels_dae 64 --ch_mult 1 1 2 2 4 4 --num_timesteps 4 \
 			--num_res_blocks 2 --batch_size 8 --num_epoch 500 --ngf 64 --embedding_type positional --use_ema --ema_decay 0.999 --r1_gamma 1. \
 			--z_emb_dim 256 --lr_d 1e-4 --lr_g 1.6e-4 --lazy_reg 10 --save_content --datadir data/lsun/ \
 			--master_port $MASTER_PORT --num_process_per_node $GPUS \
 
 	fi
 else
-
 	echo "==> Test DDGAN"
-
-	if [[ $DATASET == cifar10 ]]; then
+	if [[ $DATASET == cifar10 ]]; then \
 		python test_ddgan.py --dataset cifar10 --exp ddgan_cifar10_exp1 --num_channels 3 --num_channels_dae 128 --num_timesteps 4 \
 			--num_res_blocks 2 --nz 100 --z_emb_dim 256 --n_mlp 4 --ch_mult 1 2 2 2 --epoch_id 1200 \
-			# --compute_fid --real_img_dir pytorch_fid/cifar10_train_stat.npy \
+			--compute_fid --real_img_dir pytorch_fid/cifar10_train_stat.npy \
 			# --batch_size 100 --measure_time \
 
 	elif [[ $DATASET == stl10 ]]; then
@@ -73,20 +70,20 @@ else
 			# --batch_size 100 --measure_time \
 
 	elif [[ $DATASET == celeba_256 ]]; then
-		python3 test_ddgan.py --dataset celeba_256 --image_size 256 --exp ddgan_celebahq_exp1 --num_channels 3 --num_channels_dae 64 \
+		python test_ddgan.py --dataset celeba_256 --image_size 256 --exp ddgan_celebahq_exp1 --num_channels 3 --num_channels_dae 64 \
 			--ch_mult 1 1 2 2 4 4 --num_timesteps 2 --num_res_blocks 2  --epoch_id 550 \
 			# --compute_fid --real_img_dir pytorch_fid/celebahq_stat.npy
 			# --batch_size 100 --measure_time \
 
 	elif [[ $DATASET == celeba_512 ]]; then
-		python3 test_ddgan.py --dataset celeba_512 --image_size 512 --exp ddgan_celebahq_512_exp1 --num_channels 3 --num_channels_dae 64 \
+		python test_ddgan.py --dataset celeba_512 --image_size 512 --exp ddgan_celebahq_512_exp1 --num_channels 3 --num_channels_dae 64 \
 			--ch_mult 1 1 2 2 4 4 --num_timesteps 2 --num_res_blocks 2  --epoch_id 325 \
 			--batch_size 100 \
 			# --compute_fid --real_img_dir ./pytorch_fid/celebahq_512_stat.npy \
 			# --measure_time \
 
 	elif [[ $DATASET == lsun ]]; then
-		python3 test_ddgan.py --dataset lsun --image_size 256 --exp ddgan_lsun_exp1 --num_channels 3 --num_channels_dae 64 \
+		python test_ddgan.py --dataset lsun --image_size 256 --exp ddgan_lsun_exp1 --num_channels 3 --num_channels_dae 64 \
 			--ch_mult 1 1 2 2 4 4  --num_timesteps 4 --num_res_blocks 2  --epoch_id 500 \
 			# --compute_fid --real_img_dir pytorch_fid/lsun_church_stat.npy \
 			# --batch_size 100 --measure_time \
